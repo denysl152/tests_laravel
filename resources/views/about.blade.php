fa29139b6678c4117d75cf81d0b762e2c5110fa3
denysl152 <chabinlouves199@gmail.com>
2018-05-18 14:31:36 -0400

    i use a framework PHP

---
+++ resources/views/about.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LARAVEL-PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Kavivanar" rel="stylesheet">
+</head>
+<style>
+    body{
+        font-family: 'Kavivanar', cursive;
+        width: 70%;
+        margin-left: 250px;
+    }
+#clear{
+background-color: greenyellow;
+
+}
+.Savage{
+    display: flex;
+    list-style: none;
+    justify-content: space-between;
+}
+li{
+    border: 2px dashed blue;
+}
+</style>
+<body>
+    <header id="clear">
+    <ul class="Savage">
+        <li>Simple</li>
+        <li>Basique</li>
+        <li>Compliquée</li>
+    </ul>
+</header>
+<div>
+    <p>« Lorem ipsum dolor sit amet consectetur »: Voilà une locution latine qui parle à des millions de graphistes à travers le monde! En effet, ce texte de remplissage leur permet de tester la mise en page de leurs maquettes: sites internet, magazines, journaux, etc. avant d’y intégrer le texte définitif.
+
+            Le lorem ipsum est donc aujourd’hui très connu des gens qui gravitent autour des métiers de conception graphique. Ce qui est moins connu, par contre, c’est l’histoire de cette locution latine…
+            
+            …Car le lorem ipsum n’a pas attendu le développement d’Illustrator Inkscape ou de Photoshop The Gimp pour se faire connaître! En fait, il est utilisé depuis des siècles par des générations de typographes pour visionner leurs polices de caractères. Ils pouvaient ainsi tester les différentes écritures et faire apparaître l’espacement des caractères, leur épaisseur, etc.
+            
+            – D’accord… mais elle sort d’où, cette phrase?
+        
+            Il faut remonter à l’an 45 avant Jésus-Christ pour en trouver les origines! Il s’agit d’un extrait d’un texte de notre brave Cicéron, ce sénateur qui a défendu la République romaine face à Jules César dont on a tant parlé sur ce site. Le vrai extrait est en fait celui-ci:
+
+Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
+
+…ce qui signifie : « Personne n’aime la douleur en elle-même, ne la recherche et ne la souhaite, tout simplement parce qu’il s’agit de la douleur». Notre cher La Palice n’aurait pas dit mieux! (il faut croire que les sado-maso n’étaient pas à la mode sous Jules César…)
+
+Comment cet extrait d’un obscur traité de théorie éthique s’est-il retrouvé diffuser à des millions d’exemplaires sur les écrans de nos ordinateurs? C’est au XVIè siècle qu’un imprimeur a choisi cette phrase pour réaliser des tests sur ses typographies. Depuis, c’est resté…
+
+Quel est l’intérêt d’utiliser encore aujourd’hui cette locution? Il est multiple! Premièrement, une petite touche de patrimoine historique ne fait jamais de mal… Ensuite, il permet, d’un simple coup d’œil, d’être sûr que la page ne contient pas le texte définitif (ce qui évite des mises en impression ou en production par inadvertance). Surtout, du fait qu’il s’agisse d’une langue qu’à peu près personne ne connaît, le cerveau humain n’est pas déconcentré par le contenu du texte et peut se consacrer entièrement à l’analyse graphique.
+
+On se quitte sur une petite énigme… Saurez-vous trouver la signification de la phrase « Portez ce vieux whisky au juge blond qui fume »? Le premier qui donne la bonne réponse dans les commentaires a gagné!
+<details>Laravel c/est de la magie!!!!</details>    
+        
+        </p>
+     
+</div>
+</body>
+</html>
